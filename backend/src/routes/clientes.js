0be3a3799f41d5335dd944c8d838c9b3f94516a0
danipjzz <neifren.daniela@gmail.com>
2026-03-14 12:40:27 -0500

    cambios en rutas de clientes

--- backend/src/routes/clientes.js
+++ backend/src/routes/clientes.js
@@ -1,2 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+const clientesController = require("../controllers/clientesController");
+
 router.patch("/correo", clientesController.actualizarCorreo);
+
 module.exports = router;
