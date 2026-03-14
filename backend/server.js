282f3837e62c5b9605c27b71cc85be80d957754a
danipjzz <neifren.daniela@gmail.com>
2026-03-14 12:24:06 -0500

    cambios en orden de middleware

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,22 @@
 require('dotenv').config();
+
 const express = require('express');
-
-const pedidosRoutes = require('./src/routes/pedidos');
-
 const app = express();
 
+const pedidosRoutes = require('./src/routes/pedidos');
 const clientesRoutes = require("./src/routes/clientes");
 
-app.use("/clientes", clientesRoutes);
-
+// IMPORTANTE: primero el middleware
 app.use(express.json());
 
+// luego las rutas
+app.use("/clientes", clientesRoutes);
 app.use('/pedidos', pedidosRoutes);
 
 app.get('/', (req, res) => {
   res.send('API MIS Satori funcionando');
 });
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
